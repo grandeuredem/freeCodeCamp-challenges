3ab241ead27f535ffcb412d2818d6d0b4e3d6212
Grandeur Edem <greatgrandeuredem@gmail.com>
2026-09-02 14:16:30 +0100

    Fix day 1 tribonacci logic

--- September/01_tribonacci_sequence.py
+++ September/01_tribonacci_sequence.py
@@ -2,20 +2,13 @@ def tribonacci_sequence(start_sequence, lenght):
     '''
     this is a tribonacci sequence generator whatever that may be that was made by yours truly
     '''
-    counter = 0
     first_position = 0
     third_position = 3
     #repeat as many times as the lenght is
-    while counter < lenght:
-        #a way to break out of the loop once the desired lenght has been reached
-        if len(start_sequence) >= lenght:
-            break
-        #add the sum of the first three digits in the list of numbers
-        added_digits = sum(start_sequence[first_position:third_position])
-        #append the sum iback nto the list
-        start_sequence.append(added_digits)
+    while len(start_sequence) < lenght:
+        #add  and append the sum of the first three digits in the list of numbers
+        start_sequence.append(sum(start_sequence[first_position:third_position]))
         #increase the watch variable and shifting index accordingly
-        counter += 1
         first_position += 1
         third_position += 1
     #return the final list
