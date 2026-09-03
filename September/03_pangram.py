6a7852c07152311ac50a65f7ea3ccc5c9d4e9d7b
Grandeur Edem <greatgrandeuredem@gmail.com>
2026-09-03 13:06:24 +0100

    Day 3 pangram solution

--- September/03_pangram.py
+++ September/03_pangram.py
@@ -1,9 +1,9 @@
 def is_pangram(sentence, letters):
-    mod_sentence = ''
+    mod_sentence = set()
     sentence = sentence.lower()
     for char in sentence:
         if char.isalpha():
-            mod_sentence += char
-    return all(x in letters for x in mod_sentence) and all(x in mod_sentence for x in  letters)
+            mod_sentence.add(char)
+    return mod_sentence == set(letters)
 
 print(is_pangram('A b c a a b c-a','abc'))
